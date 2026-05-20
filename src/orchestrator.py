2a8270ca8217243b92a91ad999e42b342489a511
Abdul Mujeeb <147976573+MJ-9669@users.noreply.github.com>
2026-05-20 19:35:14 +0530

    feat: updated the orchestrator file in order for it to work with the LLM function

--- src/orchestrator.py
+++ src/orchestrator.py
@@ -1,9 +1,28 @@
 import os
+import json
 from ingestion import clone_repository, cleanup_repository
 from parser import ASTAnalyzer
+from reviewer import CodeReviewer
 
-def run_pipeline(repo_url: str):
+# def analyze_git_repo(repo_url: str) -> list:
+
+#     repo_path = None
+#     all_review_comment = []
+#     reviewer = CodeReviewer()
+
+#     try:
+#         repo_path = clone_repository(repo_url)
+
+
+
+
+
+
+def analyze_git_repo(repo_url: str) -> list:
     repo_path = None
+    all_review_comment = []
+    reviewer = CodeReviewer()
+
     try:
         repo_path = clone_repository(repo_url)
 
@@ -19,18 +38,41 @@ def run_pipeline(repo_url: str):
                         analyzer = ASTAnalyzer(full_path)
                         structures = analyzer.extract_structures()
 
-                        if not structures:
-                            print(f"No functions or class found")
-
                         for struct in structures:
-                            print(f"Found {struct['type'].upper()} : {struct['name']} (Lines{struct['start_line']}-{struct['end_line']})")
+                            print(f"  -> Sending {struct['type'].upper()} '{struct['name']}' to LLM Reviewer")
 
-                    except Exception as parse_error:
-                        print(f"Could not parse {relative_path}:: {parse_error}")
+                            raw_reviews = reviewer.review_code_chunk(
+                                file_name = relative_path,
+                                chunk_type = struct["type"],
+                                chunk_name = struct["name"],
+                                code_content = struct["code"]
+                            )
+                            
+                            for review in raw_reviews:
+                                review["file"] = relative_path,
+                                review["scope"] = struct["name"]
+
+                                if review["line_number"] is not None:
+                                    review["absolute_line"] = struct["start_line"] + review["line_number"] - 1
+                                else:
+                                    review["absolute_line"] = struct["start_line"]
+
+                                all_review_comment.append(review)
+
+                    except Exception as e:
+                        print(f"Failed processing file context for {relative_path}: {e}")
+
+        return all_review_comment
+    
     finally:
         if repo_path:
             cleanup_repository(repo_path)
 
 if __name__ == "__main__":
+
     test_repo = "https://github.com/psf/requests-html" 
-    run_pipeline(test_repo)
+    print("=== STARTING FULL AGENT CONTEXT TEST ===")
+    results = analyze_git_repo(test_repo)
+    
+    print("\n=== AGGREGATED CODE REVIEW RESULTS ===")
+    print(json.dumps(results, indent=2))
